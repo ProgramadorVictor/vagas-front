01b920ffe7835df0662c7d7ee300ad045dba1189
ProgramadorVictor <victorochaandrade@gmail.com>
2024-10-04 17:23:48 -0300

    feat: padroes de chamada de objeto literal e nomeação

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@
     É a ideia de que podemos reutilizar código com vários componentes em arquivos unicos, cada componente...
     Tendo suas funções unicas. E sendo colocado em diversas partes da estrutura HTML. Desta forma podemos encapsular e reutilizar o código.
   -->
-  <TopoComponent/>
+  <Topo/><!-- Percebe-se que ao utilizar sintaxe mais clara o component é chamado pela sua chave, não por seu nome. -->
   <ConteudoComponent/>
+  <conteudo-component/><!-- Utilização da case, kebab-case -->
+  <conteudo-kebab-case/>
 </template>
 
 <script>
@@ -15,8 +17,9 @@ import TopoComponent from './components/layouts/TopoComponent.vue'
 export default {
   name: 'App',
   components: { //Precisamos registar os components, para usar no contexto de template.
-    ConteudoComponent,
-    TopoComponent,
+    ConteudoComponent, //Sintaxe enxuta.
+    Topo: TopoComponent, //Topo é a chave e TopoComponent é o conteúdo que vai ser recebido pela chave Topo, Topo faz referencia a TopoComponent.
+    'conteudo-kebab-case': ConteudoComponent, //Abordagem pouco usada para nomeação de ccomponents.
   }
 }
 </script>
