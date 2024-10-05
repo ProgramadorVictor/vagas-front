1842d7f442b6cab62864cebb82ff85966a2c763d
ProgramadorVictor <victorochaandrade@gmail.com>
2024-10-05 14:54:24 -0300

    style: adicionando um visual ao componente home

--- src/components/layouts/TopoComponent.vue
+++ src/components/layouts/TopoComponent.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Vagas</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <a class="navbar-brand text-white" href="#">Vagas</a>
+                <nav class="navbar navbar-dark bg-dark">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </nav>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -1,7 +1,48 @@
 <template>
-    <div>
-        <h1>Home Componente</h1>
-        <p>Home: {{ $style }}</p>
+    <div class="container py-4">
+        <div class="row">
+            <div class="col">
+                <div class="h-100 p-5 bg-light border rounded-3">
+                    <h2>Pesquisar Vagas</h2>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="">Titulo da vaga</label>
+                                <input type="text" name="" id="" class="form-control" placeholder="Pesquise por palavras chaves por exemplo PHP, Pleno, Analista">
+                                <small class="form-text text-muted">Informe palavras que estejam relacionada ao titulo da vaga que você procura</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col text-center">
+                            <button class="btn btn-outline-dark mt-2">Buscar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-4">
+                <div class="h5 p-5 roudend-3 border bg-dark text-white">
+                    <p>Vagas Abertas</p>
+                    <h2>50</h2>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="h5 p-5 roudend-3 border bg-dark text-white">
+                    <p>Profissionais cadastrados</p>
+                    <h2>125</h2>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="h5 p-5 roudend-3 border bg-light text-dark">
+                    <p>Visitantes Online</p>
+                    <h2>25</h2>
+                </div>
+            </div>
+        </div>
+        <!-- <h1>Home Componente</h1>
+        <p>Home: {{ $style }}</p> -->
     </div>
 </template>
 <script>
