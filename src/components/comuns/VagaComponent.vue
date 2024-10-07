25cb1a5fc96c342d0c36a11084f4f0536f1a8f91
ProgramadorVictor <victorochaandrade@gmail.com>
2024-10-07 11:58:40 -0300

    feat: props tipagem de dados e validacao, tipando objeto em VagaComponent

--- src/components/comuns/VagaComponent.vue
+++ src/components/comuns/VagaComponent.vue
@@ -3,7 +3,8 @@
         <div class="card-header bg-dark text-white">{{ vaga.titulo }}</div>
         <div class="card-body">
             <p>
-                {{kebabCase}} <!-- Vue.js recebeu ma kebab-case e ela é chamada oomo CamelCase -->
+                {{ vaga.descricao }}
+                <!-- {{kebabCase}} Vue.js recebeu ma kebab-case e ela é chamada oomo CamelCase -->
             </p>
         </div>
         <div class="card-footer">
@@ -13,7 +14,37 @@
 </template>
 <script>
     export default {
-        props: ['vaga', 'kebab-case']
+        props: { //Fazendo a tipagem dos dados que vão ser recebidos pelo componente.
+            vaga: {
+                type: Object,
+                required: true,
+                validator (vaga){ //Recebe o valor da prop e retorna true/false dependendo da validação. Aqui estamos recebendo um objeto
+                    return ( //Valida o argumento acessando os atributos retornado de validator no caso o objeto retornou.
+                        typeof vaga.titulo === 'string' &&
+                        typeof vaga.descricao === 'string' &&
+                        typeof vaga.salario === 'number' &&
+                        typeof vaga.modalidade === 'string' &&
+                        typeof vaga.tipo === 'string' &&
+                        typeof vaga.publicacao === 'string'
+                    ); //O código abaixo é a mesma coisa que o de cima
+                    // return
+                    //     typeof vaga.titulo === 'string' && typeof vaga.descricao === 'string' &&
+                    //     typeof vaga.salario === 'string' && typeof vaga.modalidade === 'string' &&
+                    //     typeof vaga.tipo === 'string' && typeof vaga.publicacao === 'string'
+                }
+            },
+            kebabCase: {
+                type: String,
+                required: false,
+                validator(value){ //Validator recebe o proprio valor da props que foi passada. Retorna true/false, estamos recebendo uma variavel.
+                    if(value.length >= 10) return true;
+                    return false;
+                }
+            }
+        },
+        created(){
+            console.log(this.kebabCase);
+        }
     }
 </script>
 <style lang="css">
